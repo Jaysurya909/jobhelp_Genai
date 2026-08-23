509394d98c38f78ad1152db7ce13f5da07696af3
Jaysurya909 <jaysurya6969@gmail.com>
2026-08-23 19:35:26 +0530

    created the registerUserController

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,5 +1,7 @@
 const {Router} = require('express')
-
 const authRouter = Router();
+const authController = require('../controllers/auth.controller')
+
+authRouter.post('/register',authController.registerUserController)
 
 module.exports = authRouter
