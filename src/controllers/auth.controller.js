a82f89356c523ddcafcd26de1053aa2096843129
Jaysurya909 <jaysurya6969@gmail.com>
2026-08-23 19:45:01 +0530

    created the loginUserController

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -49,6 +49,43 @@ async function registerUserController(req,res) {
 
 }
 
+async function loginUserController(req,res) {
+    
+    const{email,password} = req.body;
 
+    const user = await userModel.findOne({email})
 
-module.exports = {registerUserController}
+    if(!user){
+        return res.status(400).json({
+            message:"Invalid email or password"
+        })
+    }
+
+    const isPasswordValid = bcrypt.compare(password,user.password)
+
+        if(!isPasswordValid){
+            return res.status(400).json({
+                message:"Invalid email or password"
+            })
+        }
+
+    const token = jwt.sign(
+        {id:user._id,username:user.username},
+        process.env.JWT_SECRET,
+        {expiresIn:"1d"}
+    )
+
+    res.cookie("token",token)
+
+    res.status(200).json({
+        message:"User loggedIn successfully.",
+        user:{
+            id:user._id,
+            username:user.username,
+            email:user.email
+        }
+    })
+
+}
+
+module.exports = {registerUserController,loginUserController}
--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -4,4 +4,6 @@ const authController = require('../controllers/auth.controller')
 
 authRouter.post('/register',authController.registerUserController)
 
+authRouter.post('/login',authController.loginUserController)
+
 module.exports = authRouter
